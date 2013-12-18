5d24f207f4bdff1216c7b735e1fbd70bbc3cbe89
wangdahu <553784858@qq.com>
2013-12-18 20:50:40 +0800

    test

--- source/bootstrap.inc.php
+++ source/bootstrap.inc.php
@@ -122,7 +122,7 @@ $_GPC = array_merge($_GET, $_POST, $_GPC);
 
 unset($config);
 unset($cplen);
-
+echo IA_ROOT;exit;
 defined('IN_SYS') && require IA_ROOT . '/source/bootstarp.sys.inc.php';
 defined('IN_MOBILE') && require IA_ROOT . '/source/bootstarp.mobile.inc.php';
 defined('IN_API') && require IA_ROOT . '/source/bootstarp.api.inc.php';
